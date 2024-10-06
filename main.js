aed2828ef985db688904972e45f41d932452d2cd
FAWN BARISIC <fawnbarisic@gmail.com>
2024-10-05 20:26:22 -0400

    added moisture and soil quality, layer changing functionality

--- main.js
+++ main.js
@@ -1,3 +1,98 @@
+let embeddedMap;
+let currentOverlay;
+
+function showSoil() {
+  var soilImage = ee.Image('OpenLandMap/SOL/SOL_TEXTURE-CLASS_USDA-TT_M/v02');
+
+  const soilParams = {
+    bands: ['b0'],
+    min: 1,
+    max: 12,
+    opacity: 0.75,
+    palette: [
+      'd5c36b','b96947','9d3706','ae868f','f86714','46d143',
+      '368f20','3e5a14','ffd557','fff72e','ff5a9d','ff005b',
+    ]
+  };
+  
+  // Get the map ID for the image and handle the response
+  const mapId = soilImage.getMap(soilParams);
+  const tileSource = new ee.layers.EarthEngineTileSource(mapId);
+  const overlay = new ee.layers.ImageOverlay(tileSource);
+  if (currentOverlay) {
+    embeddedMap.overlayMapTypes.pop();  // Remove the previous overlay
+  }
+
+  // Set the new overlay as the current one
+  currentOverlay = overlay;
+  embeddedMap.overlayMapTypes.push(overlay);
+}
+
+function showMoisture() {
+
+  const soilMoistureCollection = ee.ImageCollection('NASA/SMAP/SPL4SMGP/007')
+  
+  const moistRecentImage = soilMoistureCollection
+  .filterDate('2024-01-01', '2024-12-31')  // Adjust the date range as needed
+    .sort('system:time_start', false)         // Sort by time, descending
+    .first();                                 // Get the first image
+  
+
+  // Set visualization parameters
+  const moistureParams = {
+    bands: ['specific_humidity_lowatmmodlay', 'sm_surface', 'sm_rootzone'],
+    min: 0,
+    max: 1,
+    gamma: [15, 10, 5],
+    opacity: 0.75,
+  };
+  
+  // Get the map ID for the image and handle the response
+  const mapId = moistRecentImage.getMap(moistureParams);
+  const tileSource = new ee.layers.EarthEngineTileSource(mapId);
+  const overlay = new ee.layers.ImageOverlay(tileSource);
+  if (currentOverlay) {
+    embeddedMap.overlayMapTypes.pop();  // Remove the previous overlay
+  }
+
+  // Set the new overlay as the current one
+  currentOverlay = overlay;
+  embeddedMap.overlayMapTypes.push(overlay);
+
+}
+
+function showEvaporation() {
+
+  // Get the MODIS ImageCollection
+  const evaporationCollection = ee.ImageCollection("MODIS/061/MOD16A2GF");
+  // Select an image from the collection (e.g., the most recent image)
+  const evapRecentImage = evaporationCollection
+    .filterDate('2020-01-01', '2024-12-31')  // Adjust the date range as needed
+    .sort('system:time_start', false)         // Sort by time, descending
+    .first();                                 // Get the first image
+  
+  // Set visualization parameters
+  const evaporationParams = {
+    bands: ['PET', 'PLE', 'ET_QC'],
+    min: 0,
+    max: 500,
+    gamma: [0.95, 1, 0.5],
+    opacity: 0.75,
+  };
+  
+  // Get the map ID for the image and handle the response
+  const mapId = evapRecentImage.getMap(evaporationParams);
+  const tileSource = new ee.layers.EarthEngineTileSource(mapId);
+  const overlay = new ee.layers.ImageOverlay(tileSource);
+  if (currentOverlay) {
+    embeddedMap.overlayMapTypes.pop();  // Remove the previous overlay
+  }
+
+  // Set the new overlay as the current one
+  currentOverlay = overlay;
+  embeddedMap.overlayMapTypes.push(overlay);
+}
+
 function setUpMap() {
   // Hide the sign-in button.
   document.getElementById("g-sign-in").setAttribute("hidden", "true");
@@ -9,32 +104,23 @@ function setUpMap() {
   const mapContainer = document.getElementById("map");
 
   // Create an interactive map inside the placeholder DOM element.
-  const embeddedMap = new google.maps.Map(mapContainer, {
+  embeddedMap = new google.maps.Map(mapContainer, {
     // Pan and zoom initial map viewport to Grand Canyon.
     center: {lng: -112.8598, lat: 36.2841},
     zoom: 9,
-  });
+  }
+);
+  showEvaporation();
+}
 
-  // Get the MODIS ImageCollection
-  const imageCollection = ee.ImageCollection("MODIS/061/MOD16A2GF");
-  // Select an image from the collection (e.g., the most recent image)
-  const recentImage = imageCollection
-    .filterDate('2018-01-01', '2024-12-31')  // Adjust the date range as needed
-    .sort('system:time_start', false)         // Sort by time, descending
-    .first();                                 // Get the first image
-  
-  // Set visualization parameters
-  const visParams = {
-    min: 0,
-    max: 3000,
-    gamma: [0.95, 1.1, 1]
-  };
-  
-  // Get the map ID for the image and handle the response
-  const mapId = recentImage.getMap(visParams);
-  const tileSource = new ee.layers.EarthEngineTileSource(mapId);
-  const overlay = new ee.layers.ImageOverlay(tileSource);
-  embeddedMap.overlayMapTypes.push(overlay);
+function changeMap(mode) {
+  if (mode == 0) {
+    showSoil()
+  } else if (mode ==1) {
+    showMoisture();
+  } else {
+    showEvaporation();
+  }
 }
 
 // Handles clicks on the sign-in button.
